1df53d6446186640f4419e0cb2717b4dbce1fce6
Jay Perry <j@jayperryworks.com>
2020-12-03 12:49:42 -0500

    update class names to use category prefixes

---
+++ source/assets/stylesheets/tokens/font-tokens-test.js
@@ -0,0 +1,44 @@
+fonts = {
+	display: {
+		family: 'Josefin Sans',
+		stack: bourbon.$font-stack-system,
+		styles: [
+			{
+				name: 'light'
+				weight: 300,
+				style: normal
+			},
+			{
+				name: 'default'
+				weight: 400,
+				style: normal
+			},
+			{
+				name: 'bold'
+				weight: 600,
+				style: normal
+			},
+		]
+	},
+	default: {
+		family: 'Cardo',
+		stack: bourbon.$font-stack-georgia,
+		styles: [
+			{
+				name: 'normal'
+				weight: 400,
+				style: normal
+			},
+			{
+				name: 'normal italic'
+				weight: 400,
+				style: italic
+			},
+			{
+				name: 'bold'
+				weight: 700,
+				style: normal
+			}
+		]
+	}
+}
