fd85d4fae475da9f5a510e5cb943055756a7020c
Jay Perry <j@jayperryworks.com>
2020-12-09 11:30:41 -0500

    clean up homepage layouts

--- source/components/sections/Jumbotron.js
+++ source/components/sections/Jumbotron.js
@@ -2,10 +2,11 @@
 // -> a full-bleed header for a page that introduces the content below
 
 module.exports = (content, {
-	contentWidth = 'xwide'
+	contentWidth = 'xwide',
+	className = ''
 } = {}) => {
 	return `
-		<header class="u-padding-y-wide u-padding-x-outside">
+		<header class="u-padding-y-wide u-padding-x-outside ${className}">
 			<div class="
 				c-content-width ${contentWidth || ''} centered
 				u-margin-y-flow
