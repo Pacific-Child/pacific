e93b55e0f86e2d0b63493dbfbf0fd5f6d72afbbe
Jay Perry <j@jayperryworks.com>
2020-12-11 21:30:26 -0500

    pull in site config and nav data from cms

--- dato.config.js
+++ dato.config.js
@@ -1,6 +1,21 @@
 module.exports = (dato, root, i18n) => {
-	const home = dato.home
+const { home, pages, siteConfiguration } = dato
 
+	// global site config
+	root.createDataFile('source/data/site.json', 'json', {
+		organization: siteConfiguration.organizationName,
+		title: siteConfiguration.siteTitle,
+		header: {
+			logo: siteConfiguration.logo.toMap(),
+			nav: siteConfiguration.mainMenu.toMap()
+		},
+		footer: {
+			newsletterSignup: siteConfiguration.newsletterSignupBlurb,
+			copyright: siteConfiguration.copyrightNotice
+		}
+	})
+
+	// homepage
 	root.createDataFile('source/data/home.json', 'json', {
 		cover: {
 			logo: home.logo,
@@ -13,9 +28,10 @@ module.exports = (dato, root, i18n) => {
 		sections: home.sections.toMap()
 	})
 
+	// interior pages
 	root.directory('source/data/pages', (pagesDir) => {
 		// interior pages
-		dato.pages.forEach((page) => {
+		pages.forEach((page) => {
 			pagesDir.createDataFile(`${page.slug}.json`, 'json', {
 				title: page.title,
 				date: page.createdAt,
