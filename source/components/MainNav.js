bc749a8e083acef28d14e8057f8f9114c2b4e2ff
Jay Perry <j@jayperryworks.com>
2020-12-17 16:42:16 -0500

    set up icon styles

--- source/components/MainNav.js
+++ source/components/MainNav.js
@@ -5,7 +5,7 @@ const NavItem = (item, currentPage) => {
 		<li class="u-display-inline-block u-no-padding-top u-type-scale-zero">
 			<a
 				class="
-					u-type-scale-zeta u-type-font-display u-type-link-undecorated
+					u-type-scale-epsilon u-type-font-display u-type-link-undecorated
 					u-display-inline-block
 					${item.link.slug.includes(currentPage) ? 'u-border-bottom' : ''}
 				"
@@ -20,7 +20,8 @@ const NavItem = (item, currentPage) => {
 module.exports = ({
 	logo = '',
 	items = [],
-	currentPage = '/'
+	currentPage = '/',
+	breakpoint = 'medium'
 } = {}) => {
 	return `
 		<header class="u-padding-y-narrow u-padding-x-outside">
@@ -30,7 +31,7 @@ module.exports = ({
 						c-bookend-item
 						c-gutter-item
 						u-display-inline-block
-						u-type-scale-zeta u-type-font-display u-type-weight-bold u-type-link-undecorated
+						u-type-scale-epsilon u-type-font-display u-type-weight-bold u-type-link-undecorated
 						u-no-margin
 					"
 					href="/"
@@ -41,16 +42,30 @@ module.exports = ({
 						alt="Pacific Regional Council for Early Childhood Development"
 						style="max-width: 4.5rem;"
 					>
-					Pacific Regional Council for Early Childhood Development
+					<span class="u-hide-below@${breakpoint}">Pacific Regional Council for Early Childhood Development</span>
 				</a>
 				<nav class="c-bookend-item | c-gutter-item | u-display-inline-block">
 					<ul class="
 						u-display-inline-block
-						u-hide-visually u-show-visually-above@xsmall
+						u-hide-visually u-show-visually-above@${breakpoint}
 						u-margin-x-flow
 					">
 						${items.map(item => NavItem(item, currentPage)).join('')}
 					</ul>
+					<a
+						@click.prevent="navOpen = true"
+						x-show.transition.opacity="!navOpen"
+						class="u-display-inline-block | u-hide-above@${breakpoint} | u-type-font-display u-type-scale-epsilon u-type-link-undecorated"
+						href="#footer-nav"
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" class="b-icon" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+						  <line x1="4" y1="6" x2="20" y2="6" />
+						  <line x1="4" y1="12" x2="20" y2="12" />
+						  <line x1="4" y1="18" x2="20" y2="18" />
+						</svg>
+						Menu
+					</a>
 				</nav>
 			</div>
 		</header>
