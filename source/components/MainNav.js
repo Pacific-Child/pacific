2dc3a2c9f5ebd278b6581598f0b3d1d73093434e
Jay Perry <j@jayperryworks.com>
2020-12-18 12:43:37 -0500

    spacing/layout cleanup in main nav

--- source/components/MainNav.js
+++ source/components/MainNav.js
@@ -25,7 +25,9 @@ module.exports = ({
 } = {}) => {
 	return `
 		<header class="u-padding-y-narrow u-padding-x-outside">
-			<div class="c-bookend horizontal | c-gutter narrow | u-type-scale-zero">
+			<div class="c-bookend horizontal | c-gutter | u-type-scale-zero">
+
+				<!-- logo -->
 				<a
 					class="
 						c-bookend-item
@@ -37,32 +39,36 @@ module.exports = ({
 					href="/"
 				>
 					<img
-						class="u-display-inline-block u-position-vertical-align-middle u-padding-right-narrow"
+						class="u-display-inline-block u-position-vertical-align-middle u-margin-right-narrow"
 						src="/images/pacific-logo-icon.svg"
 						alt="Pacific Regional Council for Early Childhood Development"
 						style="max-width: 4.5rem;"
 					>
 					<span class="u-hide-below@${breakpoint}">Pacific Regional Council for Early Childhood Development</span>
 				</a>
+
+				<!-- main nav -->
 				<nav class="c-bookend-item | c-gutter-item | u-display-inline-block">
 					<ul class="
 						u-display-inline-block
-						u-hide-visually u-show-visually-above@${breakpoint}
+						u-hide-visually u-show-visually-above@large
 						u-margin-x-flow
 					">
 						${items.map(item => NavItem(item, currentPage)).join('')}
 					</ul>
+
+					<!-- mobile nav button (links to footer nav w/o JS) -->
 					<a
 						@click.prevent="navOpen = true"
 						x-show.transition.opacity="!navOpen"
-						class="u-display-inline-block | u-hide-above@${breakpoint} | u-type-font-display u-type-scale-epsilon u-type-link-undecorated"
+						class="u-display-inline-block | u-hide-above@large | u-type-font-display u-type-scale-epsilon u-type-link-undecorated"
 						href="#footer-nav"
 					>
 						<svg xmlns="http://www.w3.org/2000/svg" class="b-icon" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-						  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-						  <line x1="4" y1="6" x2="20" y2="6" />
-						  <line x1="4" y1="12" x2="20" y2="12" />
-						  <line x1="4" y1="18" x2="20" y2="18" />
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+							<line x1="4" y1="6" x2="20" y2="6" />
+							<line x1="4" y1="12" x2="20" y2="12" />
+							<line x1="4" y1="18" x2="20" y2="18" />
 						</svg>
 						Menu
 					</a>
