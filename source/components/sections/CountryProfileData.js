250171860d1fa39bf41d1e2705807dad2998471e
Jay Perry <j@jayperryworks.com>
2021-02-19 18:32:11 -0500

    add callout thingy to nurturing care section

--- source/components/sections/CountryProfileData.js
+++ source/components/sections/CountryProfileData.js
@@ -42,6 +42,28 @@ function ResourcesGrid (resources, className = '') {
 	`
 }
 
+function IndicatorCallout(heading, body) {
+	return `
+		<aside class="u-color-bg-well | u-border-radius | u-padding u-padding-y-flow-xnarrow">
+			<div class="c-bookend horizontal | c-gutter narrow">
+				<div class="c-bookend-item left | c-gutter-item | u-type-scale-beta | u-color-fg-highlight">
+					<svg xmlns="http://www.w3.org/2000/svg" class="b-icon xlarge" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+					  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+					  <path d="M12 9v2m0 4v.01" />
+					  <path d="M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75" />
+					</svg>
+				</div>
+				<div class="c-bookend-item right fill | c-gutter-item">
+					${ContentWrapper(`
+						<h3>${heading}</h3>
+						${Passage(Markdown(body))}
+					`)}
+				</div>
+			</div>
+		</aside>
+	`
+}
+
 // group data under a heading
 function Section (content, { title, description, resources, className } = {}) {
 	return SectionWrapper(`
@@ -329,7 +351,16 @@ function NurturingCareSection (country, indicators) {
 					number: indicators.basicSanitation
 				})}
 			</li>
-		`), { title: section.subsectionSecurityAndSafety })
+		`), { title: section.subsectionSecurityAndSafety }),
+
+		// Responsive caregiving
+		Subsection(
+			IndicatorCallout(
+				section.indicatorCalloutHeadingDataNeeded,
+				section.indicatorCalloutDescriptionDataNeeded
+			), 
+			{ title: section.indicatorSubsectionResponsiveCaregiving }
+		)
 	].join(''), { ...section })
 }
 
