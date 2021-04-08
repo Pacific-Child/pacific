f6546159e1afe3f592b0e716b20c4df93be40a36
aruni-HostCreative <aruni@host-creative.com>
2021-04-08 16:12:52 -0400

    add the country list description

--- source/components/sections/CountriesList.js
+++ source/components/sections/CountriesList.js
@@ -1,39 +1,36 @@
 // Countries List section
 // -> displays a list of countries with flags in cards
-const ContentWrapper = require('../blocks/ContentWrapper.js')
-const CountriesGrid = require('../blocks/CountriesGrid.js')
-const CountriesMap = require('../blocks/CountriesMap/index.js')
-const PictureFrame = require('../blocks/PictureFrame.js')
-const SectionWrapper = require('../blocks/SectionWrapper.js')
+const ContentWrapper = require("../blocks/ContentWrapper.js");
+const CountriesGrid = require("../blocks/CountriesGrid.js");
+const CountriesMap = require("../blocks/CountriesMap/index.js");
+const PictureFrame = require("../blocks/PictureFrame.js");
+const SectionWrapper = require("../blocks/SectionWrapper.js");
+const Markdown = require("../blocks/Markdown.js");
+const Passage = require("../blocks/Passage.js");
 
-const breakpoint = 'medium'
+const breakpoint = "medium";
 
-function Countries (countries) {
+function Countries(countries) {
 	return [
 		CountriesGrid(countries, {
-			className: `u-hide-above@${breakpoint}`
+			className: `u-hide-above@${breakpoint}`,
 		}),
-		PictureFrame(
-			CountriesMap(countries),
-			{
-				display: 'block',
-				className: `u-hide-below@${breakpoint}`
-			}
-		)
-	].join('')
+		PictureFrame(CountriesMap(countries), {
+			display: "block",
+			className: `u-hide-below@${breakpoint}`,
+		}),
+	].join("");
 }
 
-module.exports = ({
-	title,
-	countries = []
-}) => {
+module.exports = ({ title, description, countries = [] }) => {
 	return countries.length > 0
-		? SectionWrapper(`
-				${title ? `<h2 class="u-type-align-center">${title}</h2>` : ''}
-				${ContentWrapper(
-					Countries(countries),
-					{ width: 'xwide' }
-				)}
-			`, { className: 'u-margin-y-flow-wide' })
-		: ''
-}
+		? SectionWrapper(
+				`
+				${title ? `<h2 class="u-type-align-center">${title}</h2>` : ""}
+				${description ? Passage(Markdown(description)) : ""}
+				${ContentWrapper(Countries(countries), { width: "xwide" })}
+			`,
+				{ className: "u-margin-y-flow-wide" }
+		  )
+		: "";
+};
