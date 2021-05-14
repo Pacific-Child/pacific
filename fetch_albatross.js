a1e56b5b365edde06d3e112346785d3ceaef74d2
Abdullah Norozi Iranzad <a1norozi@gmail.com>
2021-05-14 06:59:22 -0600

    Generate data for all countries

--- fetch_albatross.js
+++ fetch_albatross.js
@@ -8,6 +8,8 @@ const fs = require('fs')
 !fs.existsSync('source/data/albatross') && fs.mkdirSync('source/data/albatross');
 const url = "http://api.pacific-child.org/graphql";
 
+const countryArray = ['FJI', 'KIR', 'NIU', 'COK', 'FSM', 'WSM', 'NRU', 'PLW', 'PNG', 'MHL', 'SLB', 'TKL', 'TON', 'TUV', 'VUT']
+
 const queryTopics = `
     query {
         topics {
@@ -31,47 +33,50 @@ const optsTopics = {
                             .catch(console.error);
     const forLoop = async _ => {
         console.log('Start')
-        const restructuredData = []
-        for (let index = 0; index < topics.length; index++) {
-            await fetch(
-                url,
-                {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ query: `
-                        query {
-                            dataPointValuesForTopic(countryCode: "FJI", topicId: ${topics[index].id}){
-                                country
-                                dataPoint {
-                                    id
-                                    body
-                                    csvColumnName
-                                    name
+        for (let cindex = 0; cindex < countryArray.length; cindex++) {
+            console.log(countryArray[cindex])
+            const restructuredData = []
+            for (let index = 0; index < topics.length; index++) {
+                await fetch(
+                    url,
+                    {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ query: `
+                            query {
+                                dataPointValuesForTopic(countryCode: "${countryArray[cindex]}", topicId: ${topics[index].id}){
+                                    country
+                                    dataPoint {
+                                        id
+                                        body
+                                        csvColumnName
+                                        name
+                                    }
                                 }
                             }
-                        }
-                    ` })
-                }
-            )
-                .then(res => res.json())
-                .then(res => {
-                    const data = res.data.dataPointValuesForTopic
-                    for (let index = 0; index < data.length; index++) {
-                        const obj = data[index];
-                        restructuredData.push({
-                            value: obj.country,
-                            dataPointName: obj.dataPoint.csvColumnName,
-                            dataPoint: obj.dataPoint
-                        })
+                        ` })
                     }
-                    return restructuredData
-                })
-                .catch(console.error);
+                )
+                    .then(res => res.json())
+                    .then(res => {
+                        const data = res.data.dataPointValuesForTopic
+                        for (let index = 0; index < data.length; index++) {
+                            const obj = data[index];
+                            restructuredData.push({
+                                value: obj.country,
+                                dataPointName: obj.dataPoint.csvColumnName,
+                                dataPoint: obj.dataPoint
+                            })
+                        }
+                        return restructuredData
+                    })
+                    .catch(console.error);
+            }
+            console.log(restructuredData)
+            fs.writeFile(`source/data/albatross/countries_${countryArray[cindex]}.json`, JSON.stringify(restructuredData), (error) => {
+                if (error) throw error
+            })
         }
-        console.log(restructuredData)
-        fs.writeFile(`source/data/albatross/countries_FJI.json`, JSON.stringify(restructuredData), (error) => {
-            if (error) throw error
-        })
         console.log('End')
     }
     forLoop()
