c0bef1bdad28df6b96323d5f9f3ee2f4e99d2dac
Kevin Pratt <kevin@paradem.co>
2023-02-14 21:54:15 -0700

    Dunno why this was on http.
    Fetch does not honor the original method when a redirect happens.  Was
    SSL added to this project recently?

--- fetch_albatross.js
+++ fetch_albatross.js
@@ -3,12 +3,29 @@
 // 2. https://moonhighway.com/fetching-data-from-a-graphql-api
 
 // https://github.com/node-fetch/node-fetch
-const fetch = require('node-fetch')
-const fs = require('fs')
-!fs.existsSync('source/data/albatross') && fs.mkdirSync('source/data/albatross');
-const url = "http://api.pacific-child.org/graphql";
+const fetch = require("node-fetch");
+const fs = require("fs");
+!fs.existsSync("source/data/albatross") &&
+	fs.mkdirSync("source/data/albatross");
+const url = "https://api.pacific-child.org/graphql";
 
-const countryArray = ['FJI', 'KIR', 'NIU', 'COK', 'FSM', 'WSM', 'NRU', 'PLW', 'PNG', 'MHL', 'SLB', 'TKL', 'TON', 'TUV', 'VUT']
+const countryArray = [
+	"FJI",
+	"KIR",
+	"NIU",
+	"COK",
+	"FSM",
+	"WSM",
+	"NRU",
+	"PLW",
+	"PNG",
+	"MHL",
+	"SLB",
+	"TKL",
+	"TON",
+	"TUV",
+	"VUT",
+];
 
 const queryTopics = `
     query {
@@ -19,30 +36,29 @@ const queryTopics = `
 `;
 
 const optsTopics = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query: queryTopics })
+	method: "POST",
+	headers: { "Content-Type": "application/json" },
+	body: JSON.stringify({ query: queryTopics }),
 };
 
 (async () => {
-    const topics = await fetch(url, optsTopics)
-                            .then(res => res.json())
-                            .then(res => {
-                                return res.data.topics
-                            })
-                            .catch(console.error);
-    const forLoop = async _ => {
-        console.log('Start')
-        for (let cindex = 0; cindex < countryArray.length; cindex++) {
-            console.log(countryArray[cindex])
-            const restructuredData = []
-            for (let index = 0; index < topics.length; index++) {
-                await fetch(
-                    url,
-                    {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({ query: `
+	const topics = await fetch(url, optsTopics)
+		.then((res) => res.json())
+		.then((res) => {
+			return res.data.topics;
+		})
+		.catch(console.error);
+	const forLoop = async (_) => {
+		console.log("Start");
+		for (let cindex = 0; cindex < countryArray.length; cindex++) {
+			console.log(countryArray[cindex]);
+			const restructuredData = [];
+			for (let index = 0; index < topics.length; index++) {
+				await fetch(url, {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						query: `
                             query {
                                 dataPointValuesForTopic(countryCode: "${countryArray[cindex]}", topicId: ${topics[index].id}){
                                     country
@@ -56,29 +72,33 @@ const optsTopics = {
                                     }
                                 }
                             }
-                        ` })
-                    }
-                )
-                    .then(res => res.json())
-                    .then(res => {
-                        const data = res.data.dataPointValuesForTopic
-                        for (let index = 0; index < data.length; index++) {
-                            const obj = data[index];
-                            restructuredData.push({
-                                value: obj.country,
-                                dataPointName: obj.dataPoint.csvColumnName,
-                                dataPoint: obj.dataPoint
-                            })
-                        }
-                        return restructuredData
-                    })
-                    .catch(console.error);
-            }
-            fs.writeFile(`source/data/albatross/countries_${countryArray[cindex]}.json`, JSON.stringify(restructuredData), (error) => {
-                if (error) throw error
-            })
-        }
-        console.log('End')
-    }
-    forLoop()
+                        `,
+					}),
+				})
+					.then((res) => res.json())
+					.then((res) => {
+						const data = res.data.dataPointValuesForTopic;
+						for (let index = 0; index < data.length; index++) {
+							const obj = data[index];
+							restructuredData.push({
+								value: obj.country,
+								dataPointName: obj.dataPoint.csvColumnName,
+								dataPoint: obj.dataPoint,
+							});
+						}
+						return restructuredData;
+					})
+					.catch(console.error);
+			}
+			fs.writeFile(
+				`source/data/albatross/countries_${countryArray[cindex]}.json`,
+				JSON.stringify(restructuredData),
+				(error) => {
+					if (error) throw error;
+				}
+			);
+		}
+		console.log("End");
+	};
+	forLoop();
 })();
